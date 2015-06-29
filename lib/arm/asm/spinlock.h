02d4746e8faa83c42a89e53c3e86fcd957bf7230
Andrew Jones <drjones@redhat.com>
2015-06-29 21:51:10 +0200

    spinlocks: add spin_lock_init (really for reinit)
    Signed-off-by: Andrew Jones <drjones@redhat.com>

--- lib/arm/asm/spinlock.h
+++ lib/arm/asm/spinlock.h
@@ -5,6 +5,11 @@ struct spinlock {
 	int v;
 };
 
+static inline void spin_lock_init(struct spinlock *lock)
+{
+	lock->v = 0;
+}
+
 extern void spin_lock(struct spinlock *lock);
 extern void spin_unlock(struct spinlock *lock);
 
--- lib/x86/asm/spinlock.h
+++ lib/x86/asm/spinlock.h
@@ -5,6 +5,11 @@ struct spinlock {
     int v;
 };
 
+static inline void spin_lock_init(struct spinlock *lock)
+{
+	lock->v = 0;
+}
+
 void spin_lock(struct spinlock *lock);
 void spin_unlock(struct spinlock *lock);
 
